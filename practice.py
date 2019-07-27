21f532a55b37586cef6c3efa70469b59275ec45d
Ben Sweetland <bensweetland@Bens-MacBook-Pro.local>
2019-07-27 18:25:21 +0100

    more arrithmatic

--- practice.py
+++ practice.py
@@ -31,10 +31,23 @@ print sentence
 print (3+2)
 print (3-2)
 print (3*2)
-print (3**2)
+print (3**2.555)
 
 standard_operations = (2+3)*4
 print standard_operations
 
 standard_operations2 = 2+3*4
 print standard_operations2
+
+print(0.1+0.1)
+print(0.1+0.2)
+print(3*0.9999)
+
+print(4/2)
+print(12/5.7)
+
+print 12+1
+
+addition_statement = "The value of 12 plus 1 is 13"
+
+print addition_statement
