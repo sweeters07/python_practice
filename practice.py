d552124ca7c86d911afecb1bb688bded54d36d74
Ben Sweetland <bensweetland@Bens-MacBook-Pro.local>
2019-07-27 18:06:11 +0100

    added statements and numbers

--- practice.py
+++ practice.py
@@ -13,3 +13,28 @@ print full_name.lower()
 quote = "Paul Burgess once said, 'Pizza is like sex, even if it is bad its still pretty good'"
 
 print  quote
+
+
+first_name2 = 'chloe'
+last_name2 =  'ferguson'
+
+full_name2 = first_name2.title() + ' ' + last_name2.title()
+
+print full_name2
+
+statement = "is my girlfriend"
+
+sentence = full_name2 + ' ' + statement
+
+print sentence
+
+print (3+2)
+print (3-2)
+print (3*2)
+print (3**2)
+
+standard_operations = (2+3)*4
+print standard_operations
+
+standard_operations2 = 2+3*4
+print standard_operations2
